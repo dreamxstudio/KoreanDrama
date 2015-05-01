e654bb3e1c9cf662f6a9881f83f2785862a77280
dreamxstudio <dreamware@outlook.com>
2015-05-01 21:35:03 +0800

    setup

---
+++ 2007-2014/[编号078]田禹治.txt.js
@@ -0,0 +1,27 @@
+
+		ie = new ActiveXObject("InternetExplorer.Application");
+		ie.visible = true; 
+		ie.navigate("http://pan.baidu.com/s/1gdAemS7");
+		while(ie.busy){ WScript.sleep(2000);}
+		var document = ie.document;
+		var window = document.parentWindow;
+		var form = document.forms[0];
+
+		if(form)
+		{
+		  document.getElementsByName("accessCode")[0].value="6vfz"; 
+		  document.accessForm.target = "_self";
+		  document.getElementById("submitBtn").click();
+		}
+		  
+		WScript.sleep(4000);
+		for(var i=0;i<document.all.length;++i)
+		{ 
+			if(document.all[i].className == "chk-ico")
+			{
+				document.all[i].click();
+				break;
+			}
+		}  
+		document.getElementById("downFileButton").click(); 
+        
